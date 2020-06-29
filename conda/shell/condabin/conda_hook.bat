e77bac8002ba2a053bb6b197143456cd6ca5f128
Martin Prüsse <prusse.martin@gmail.com>
2020-06-29 17:15:22 -0300

    WIP TEST

--- conda/shell/condabin/conda_hook.bat
+++ conda/shell/condabin/conda_hook.bat
@@ -3,17 +3,17 @@
 @REM The file name is conda_hook.bat rather than conda-hook.bat because conda will see
 @REM the latter as a 'conda hook' command.
 
-@IF DEFINED CONDA_SHLVL GOTO :EOF
+IF DEFINED CONDA_SHLVL GOTO :EOF
 
-@FOR %%F in ("%~dp0") do @SET "__condabin_dir=%%~dpF"
-@SET "__condabin_dir=%__condabin_dir:~0,-1%"
-@SET "PATH=%__condabin_dir%;%PATH%"
-@SET "CONDA_BAT=%__condabin_dir%\conda.bat"
-@FOR %%F in ("%__condabin_dir%") do @SET "__conda_root=%%~dpF"
-@SET "CONDA_EXE=%__conda_root%Scripts\conda.exe"
-@SET __condabin_dir=
-@SET __conda_root=
+FOR %%F in ("%~dp0") do @SET "__condabin_dir=%%~dpF"
+SET "__condabin_dir=%__condabin_dir:~0,-1%"
+SET "PATH=%__condabin_dir%;%PATH%"
+SET "CONDA_BAT=%__condabin_dir%\conda.bat"
+FOR %%F in ("%__condabin_dir%") do @SET "__conda_root=%%~dpF"
+SET "CONDA_EXE=%__conda_root%Scripts\conda.exe"
+SET __condabin_dir=
+SET __conda_root=
 
-@DOSKEY conda="%CONDA_BAT%" $*
+DOSKEY conda="%CONDA_BAT%" $*
 
-@SET CONDA_SHLVL=0
+SET CONDA_SHLVL=0
